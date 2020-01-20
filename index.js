1749089b4dd31eb49a26d382acdebef067666980
ppoliani <ppoliani@gmail.com>
2020-01-20 12:09:27 +0000

    write node config files to disk

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+#!/usr/bin/env node
+const yargs = require('yargs');
+const {mkdir, readFile, writeFile} = require('fs');
+const {promisify} = require('util');
 const {createECDH} = require('crypto');
-const {privateToPublic} = require('ethereumjs-util');
+const {publicToAddress, privateToPublic} = require('ethereumjs-util');
+
+const mkdirAsync = promisify(mkdir);
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+
+const {argv} = yargs
+  .usage('Usage: $0 --config-file=[string] --to=[string] --private-key-file-name=[string]')
+  .demandOption(['configFile', 'to', 'privateKeyFileName']);
+
+const {configFile, to, privateKeyFileName} = argv;
+let _nodes = [];
+
+const execute = async () => {
+  const config = await loadConfig();
+
+  setupNodes(config.blockchain.nodes.count);
+  await mkdirAsync(`${__dirname}/${to}`);
+  await Promise.all(_nodes.map(saveKeyPair))
+  await saveGenesisFile(config.genesis);
+}
+
+const setupNodes = count => {
+  _nodes = Array.from(new Array(count), () => generateKeyPair())
+}
 
 const generateKeyPair = () => {
   const ecdh = createECDH('secp256k1');
@@ -8,9 +36,39 @@ const generateKeyPair = () => {
   const privKey = prefix(ecdh.getPrivateKey('hex', 'compressed'));
   const pubKey = privateToPublic(privKey);
 
-  return {privKey, pubKey}
+  return {
+    privKey, 
+    pubKey: prefix(pubKey.toString('hex')),
+    nodeAddress: prefix(publicToAddress(pubKey).toString('hex'))
+  }
 }
 
 const prefix = key => `0x${key}`;
 
-const {privKey, pubKey} = generateKeyPair();
+const loadConfig = async () => {
+  const config = await readFileAsync(configFile, 'utf8');
+  return JSON.parse(config);
+}
+
+const createFileContent = data => new Uint8Array(Buffer.from(data));
+
+const getNodeDir = nodeAddress => `${__dirname}/${to}/${nodeAddress}`
+
+const saveKeyPair = async ({privKey, pubKey, nodeAddress}) => {
+  const nodeDir = getNodeDir(nodeAddress);
+
+  await mkdirAsync(nodeDir);
+  await writeFileAsync(`${nodeDir}/${privateKeyFileName}`, createFileContent(privKey));
+  await writeFileAsync(`${nodeDir}/key.pub`, createFileContent(pubKey.toString('hex')));
+}
+
+const saveGenesisFile = async genesis => {
+  genesis.extraData.replace('<signers>', getSignerAddresses());
+  await writeFileAsync(`${__dirname}/${to}/genesis.json`, JSON.stringify(genesis, null, 4));
+}
+
+const getSignerAddresses = () => _nodes.map(n => n.nodeAddress).join('');
+
+(async () => {
+  await execute();
+})()
